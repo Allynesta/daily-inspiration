e3114cae9dc5e076b6047ca8c348ed97028b6e2a
Allynesta <22230923+Allynesta@users.noreply.github.com>
2026-02-24 01:24:06 +0400

    improve css of application for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import ImageBanner from './components/ImageBanner'
-import QuoteBox from './components/QuoteBox'
-import DailyTarget from './components/DailyTarget';
+import ImageBanner from "./components/ImageBanner";
+import QuoteBox from "./components/QuoteBox";
+import DailyTarget from "./components/DailyTarget";
 import TargetHistory from "./components/TargetHistory";
 import StreakCounter from "./components/StreakCounter";
 
-
 function App() {
-  return (
-    <div className="app-container">
-      <ImageBanner />
-      <div className="title">✨ One Thing Today</div>
-      <p style={{ marginTop: "-10px", color: "#666" }}>
-        Start your day with focus. Do one thing that matters.
-      </p>
-      <StreakCounter />
-      <QuoteBox />
-      <DailyTarget />
-      <TargetHistory />
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<ImageBanner />
+			<div className="title">✨ One Thing Today</div>
+			<p
+				style={{
+					marginTop: "-10px",
+					color: "#666",
+					fontSize: "clamp(13px, 3vw, 15px)",
+					padding: "0 12px",
+				}}
+			>
+				Start your day with focus. Do one thing that matters.
+			</p>
+			<StreakCounter />
+			<QuoteBox />
+			<DailyTarget />
+			<TargetHistory />
+		</div>
+	);
 }
 
 export default App;
